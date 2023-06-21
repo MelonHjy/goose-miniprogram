fc2ea4c9765f6ac484c6ccd6f64669dcabb2699c
xuejh <243697131@qq.com>
2023-06-21 10:12:14 +0800

    去掉config配置

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,16 +3,16 @@ const config = {
   TITLE: '以喵传喵',
 
   // 请求接口 base url
-  API_BASE_URL: 'https://baihuilian-dev.dreamkey.cn/api',
+  API_BASE_URL: '',
 
   // 文件上传 url （绝对路径）
-  UPLOAD_FILE_URL: 'https://baihuilian-dev.dreamkey.cn/api/document/upload',
+  UPLOAD_FILE_URL: '',
 
   // 资源路径 （云端图片、云端文件）
-  ASSETS_BASE_URL: 'https://baihuilian-dev.dreamkey.cn',
+  ASSETS_BASE_URL: '',
 
   // WebSocket base url
-  WS_BASE_URL: 'wss://baihuilian-dev.dreamkey.cn/api',
+  WS_BASE_URL: '',
 
   // TOKEN 键名
   TOKEN_KEY: 'Client-Token'
